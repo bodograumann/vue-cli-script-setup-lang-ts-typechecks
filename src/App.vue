1423952ca1ed532efb81386b002ea9fb38e24730
Bodo Graumann <mail@bodograumann.de>
2021-10-01 12:42:34 +0200

    Add wrongly typed code

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <h1>Welcome to Your Vue.js + TypeScript App</h1>
+  <p>{{ x }}</p>
 </template>
+
+<script setup lang="ts">
+const x: string = 42;
+</script>
